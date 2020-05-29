39c801b1ba2bdba5e2511faa00c37d399cfe252b
Thomazino <36938602+Thomazino@users.noreply.github.com>
2020-05-29 09:30:01 +0300

    Its not over yet

--- app/src/main/java/com/example/helloapp/Info.java
+++ app/src/main/java/com/example/helloapp/Info.java
@@ -21,7 +21,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("COVID-19 is caused by a coronavirus called SARS-CoV-2. Coronaviruses are a large family of viruses that are common in people and may different species of animals, including camels, cattle, cats, and bats.  Rarely, animal coronaviruses can infect people and then spread between people. This occurred with MERS-CoV and SARS-CoV, and now with the virus that causes COVID-19. The SARS-CoV-2 virus is a betacoronavirus, like MERS-CoV and SARS-CoV. All three of these viruses have their origins in bats.However, the exact source of this virus is unknown.");
-                cloud.setLines(21);
+                cloud.setLines(27);
 
             }
         });
@@ -34,7 +34,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("The virus that causes COVID-19 is thought to spread mainly from person to person, mainly through respiratory droplets produced when an infected person coughs or sneezes. These droplets can land in the mouths or noses of people who are nearby or possibly be inhaled into the lungs. Spread is more likely when people are in close contact with one another .COVID-19 seems to be spreading in the community spread in many affected geographic areas that people have been infected with the virus, including some who are not sure how or where they became infected.");
-                cloud.setLines(21);
+                cloud.setLines(27);
             }
         });
 
@@ -46,7 +46,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("Generally coronaviruses survive for shorter periods at higher temperatures and higher humidity than in cooler or dryer environments. However, we don’t have direct data for this virus, nor do we have direct data for a temperature-based cutoff for inactivation at this point. The necessary temperature would also be based on the materials of the surface, the environment, etc.It is not yet known either if the weather and temperature affect the spread of COVID-19 like cold and flu.There is much more to learn about the transmissibility, severity, and other features associated with COVID-19.");
-                cloud.setLines(21);
+                cloud.setLines(27);
             }
         });
 
@@ -58,7 +58,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing, or sneezing.Avoid close contact with people who are sick, even inside your home.Also,put distance between yourself and other people outside of your home.Cover your mouth and nose with a cloth face cover when around others.For the end,clean AND disinfect frequently touched surfaces daily. This includes tables, doorknobs, light switches, countertops, handles, desks, phones, keyboards, toilets, faucets, and sinks.");
-                cloud.setLines(21);
+                cloud.setLines(27);
             }
         });
 
@@ -70,7 +70,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("Not everyone needs this test.Most people will have mild illness and can recover at home without medical care and may not need to be tested.Decisions about testing are made by state and localexternal icon health departments or healthcare providers. If you have symptoms of COVID-19 and want to get tested, call your healthcare provider first. You can also visit your state or local health department’s website to look for the latest local information on testing. Although supplies of tests are increasing, it may still be difficult to find a place to get tested.");
-                cloud.setLines(21);
+                cloud.setLines(27);
             }
         });
 
@@ -82,7 +82,7 @@ public class Info extends AppCompatActivity {
                 scroll.fullScroll(ScrollView.FOCUS_UP);
                 TextView cloud=(TextView) findViewById(R.id.text1);
                 cloud.setText("If you are sick,stay always at home unless there is a serious reason to go out.Older adults and people of any age with serious underlying medical conditions should call a health care provider as soon as symptoms start.Separate yourself from other people in your home. Avoid sharing household items,such as cups and towels.Wash items thoroughly after using them with soap and water.Disinfect high-touch surfaces daily in your sick room and bathroom. Have a healthy household member do the same for surfaces in other parts of the home and bring whatever you need from the outside.");
-                cloud.setLines(21);
+                cloud.setLines(27);
             }
         });
 
